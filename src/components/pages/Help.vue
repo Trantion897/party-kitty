abc03464271fa2317fed73a53d09545e73445cb0
Peter Copeland <git@mailbox.dphin.co.uk>
2024-05-24 18:15:11 +0100

    Reword help page to be clearer

--- src/components/pages/Help.vue
+++ src/components/pages/Help.vue
@@ -26,31 +26,32 @@ const updateAmount = function(value) {
             Money can be added or removed from the store, which is synced to the server. A random name will be generated, which anyone can use to access the same store.
         </p>
         <h4>Basic use</h4>
+        <dl>
+            <dt>Split</dt>
+            <dd>Share money equally between all players with an optional share to the shared kitty</dd>
+            <dt>Add</dt>
+            <dd>Add money directly to the kitty</dd>
+            <dt>Take</dt>
+            <dd>Take money from the kitty</dd>
+        </dl>
+        <p>Enter any number of items in a single transaction; they will all be added up and converted to the largest possible denomination.</p>
         <p>
-            Choose <i>Split</i> to share money equally between all players with an optional share to the shared kitty.
-            <i>Add</i> to add money directly to the kitty, and <i>Take</i> to take money from the kitty.
-        </p>
-        <p>
-            You can enter any number of entries at once, for example if you find "300 gold, 200 silver; plus gems worth 100 gold & ornaments worth 150 gold",
-            you might make one entry for the coins, one for the gems, and one for the ornaments.
-            All of these entries will be added up, and everything converted to the largest possible denomination of the currency.
-        </p>
-        <p>
-            If optional currencies are available, these can be toggled for each transaction.
-            There may also be options to enable automatic conversion: from the optional currency to a normal currency, and/or vice-versa.
-            If automatic conversion <i>from</i> this currency is enabled, it will be converted to the relevant base currency; and may be converted to other denominations by the normal rules.
-            Otherwise, the optional currency will not be converted.
-            If automatic conversion <i>to</i> this currency is enabled, money in the standard currencies can be converted to the optional currency under the normal rules.
+            Optional currencies may be available, which are considered to be currencies that are less often used, and may or may not be convertible to other currencies.
+            The currencies can be toggled for each transaction, and if conversion is possible to or from other currencies, this can also be toggled (unless it is always enabled).
         </p>
         <h5>Splitting money</h5>
-        <p>
-            When splitting money between players, the total of all entries are added up first. The coins are then distributed between the players and the shared kitty; coins will not be divided.
-            The remainder of the division will be given to the players or the shared kitty as applicable.
-            Therefore it's possible that some money will go to the kitty even if you set the kitty proportion to zero.
-        </p>
+        <ol>
+            <li>The total of all entries are added up and converted to the largest applicable currency</li>
+            <li>Coins are distributed between the players and the shared kitty; coins will not be split or converted to a smaller currency.</li>
+            <li>The remainder of the division will be given to the players or the shared kitty as applicable.</li>
+        </ol>
+        <ul>
+            <li>It's possible that some money will go to the kitty even if you set the kitty proportion to zero.</li>
+            <li>If the split is equal, the amount going to the players and the kitty may not be equal if it can't be divided equally.</li>
+        </ul>
         <h4>Saving & syncing</h4>
         <p>
-            When you add a transaction to the kitty, it will be saved to the server.
+            Transactions are saved to the server as soon as they are added to the kitty.
             If you're creating a new kitty, a name will be generated and will appear in the kitty name field.
             You can then share this name with anyone else who need to access the same kitty.
             <strong>Only the total amount in the shared kitty is stored; not individual transactions or the amount each player has.</strong>
